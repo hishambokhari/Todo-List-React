ce4cc395dcb8e447815df5f897073cb6c9f2da59
hishambokhari <hish.b92@gmail.com>
2020-08-20 17:13:47 +0100

    Add: State

--- todo_app/src/App.js
+++ todo_app/src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [todos, setTodos] = useState(['Take the rubbish out', 'Dogs have to go for a walk']);
+
+
   return (
     <div>
       <h1>Hello World</h1>
+      <input />
+      <button>Add Todo</button>  
+      
+
+      <ul>
+        {todos.map(todo => (
+          <li>{todo}</li>
+        ))}
+      </ul>
+
     </div>
   );
 }
